d40871071ed294c5cc0a4862b336d1b26243b79c
Ruskin Kollin <ruskin.kollin@gmail.com>
2021-03-27 16:40:08 +0200

    changes made

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
